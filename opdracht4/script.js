6c13234b4d853fa6302502c1c36390d985b383e5
Dylan-Knol <dylanknol23@gmail.com>
2026-01-25 12:33:51 +0100

    DOM

---
+++ opdracht4/script.js
@@ -0,0 +1,31 @@
+let button = document.querySelector("#button");
+
+let arr = [];
+let textElement = document.querySelector("#text");
+
+let button2 = document.querySelector("#button2");
+button.addEventListener("click", () => {
+        countNumber(1);
+});
+button.addEventListener("click", waardes);
+
+function waardes() {
+    arr = [];
+    for (let i = 0; i < 5; i++) {
+        arr.push(Math.floor(Math.random() * 6) + 1);
+    }
+    console.log(arr);
+
+    textElement.textContent = arr;
+}
+
+ function countNumber(nummer){
+    let aantal = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] == nummer){
+             aantal++
+         }
+    }
+    console.log(aantal);
+
+}
